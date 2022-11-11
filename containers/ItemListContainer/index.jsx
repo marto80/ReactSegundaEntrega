3bb41c3c1b540e9b5dc49163269f5638f0a750d3
marto80 <marturanga@gmail.com>
2022-11-10 23:33:38 -0300

    Codigo mas pulido

--- containers/ItemListContainer/index.jsx
+++ containers/ItemListContainer/index.jsx
@@ -12,49 +12,6 @@ export default function ItemListContainer ({greeting}) {
 
     console.log(categoryId);
 
-    // const [nombre, setNombre] = useState("Oscar")
-    // const [fecha, setFecha] = useState("")
-
-    // const handleChangeName = () => {
-    //     if (nombre === "Oscar") setNombre("Raul")
-    //     else setNombre("Oscar")
-    //     // console.log(nombre)
-    // }
-
-    //Montaje
-    // useEffect(()=> {
-    //     //Dentro de esta función haremos todo lo que es obtención de datos desde una API
-    //     //o por ejemplo settear el carrito utilizando el localStorage
-    //     console.log("Se ejecuta solamente al montaje")
-    // }, [])
-
-    //Actualización
-    // useEffect(()=> {
-    //     //Este effect se ejecuta únicamente al cambiar el valor del state "nombre" y también a la hora del montaje.
-    //     console.log("Se cambia el nombre y/o se monta el componente")
-    //     setFecha(new Date().toLocaleString())
-    // }, [fecha, setFecha])
-
-    // const handleUpdateDate = () => {
-    //     setFecha(new Date().toLocaleString())
-    // }
-
-    /* useEffect(()=> {
-        (async ()=> {
-
-            const obtenerProductos = () => {
-                return new Promise((resolve, reject) => {
-                    setTimeout(() => {
-                        resolve(rawProducts);
-                    }, 3000);
-                });
-            }
-            
-            const response = await obtenerProductos();
-            console.log(response);
-            setProducts(response)
-        })()
-    }, []) */
 
     useEffect(()=> {
         ( async ()=> {
@@ -78,13 +35,6 @@ export default function ItemListContainer ({greeting}) {
 
     return (
         <>
-            {/* <div className='item-list-container'>
-                <h2>{greeting}</h2>
-                <p>{nombre}</p>
-                <button onClick={handleChangeName}>Cambiar nombre</button>
-                <p>{fecha}</p>
-                <button onClick={handleUpdateDate}>Actualizar la fecha</button>
-            </div> */}
             <ItemList products={products}/>
         </>
     )
